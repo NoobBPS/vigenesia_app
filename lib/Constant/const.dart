f23eebc226f404be1124dc50535a167e69a149b1
Gray0Boy <86937285+NoobBPS@users.noreply.github.com>
2024-12-14 22:12:30 +0700

    Update const.dart

--- lib/Constant/const.dart
+++ lib/Constant/const.dart
@@ -1,5 +1,1 @@
-//String url = "http://192.168.43.185";
-String url = "https://66a2-103-108-130-37.ngrok-free.app/vigenesia";
-// String url = "http://192.168.1.9";
-
-// Change This For Different IP
+String baseurl = 'http://localhost/vigenesia';
