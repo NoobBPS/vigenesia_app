2daef7c44abc9256dc278fd1574c44ccc4a32e2c
Gray0Boy <86937285+NoobBPS@users.noreply.github.com>
2025-01-11 18:09:23 +0700

    Update MainScreens.dart

--- lib/Screens/MainScreens.dart
+++ lib/Screens/MainScreens.dart
@@ -1,6 +1,4 @@
-
-
-  // ignore_for_file: non_constant_identifier_names, use_build_context_synchronously, avoid_print
+// ignore_for_file: non_constant_identifier_names, use_build_context_synchronously, avoid_print
 
 import 'dart:convert';
 
@@ -9,6 +7,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+// ignore: unused_import
 import 'package:vigenesia/Screens/EditPage.dart';
 import 'package:vigenesia/Screens/Login.dart';
 import '../Models/Motivasi_Model.dart';
@@ -257,44 +256,42 @@ class _MainScreenState extends State<MainScreens> {
                                           ),
                                         ],
                                       ),
-                                      Row(
-                                        children: [
-                                          TextButton(
-                                            child: Icon(Icons.edit, color: Colors.yellow),
-                                            onPressed: () {
-                                              Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                  builder: (BuildContext context) =>
-                                                      EditPage(
-                                                        id: item.id,
-                                                        isiMotivasi: item.isiMotivasi,
-                                                        userid: '',
-                                                        idMotivasi: '',
-                                                      ),
-                                                  ));
-                                            },
-                                          ),
-                                          TextButton(
-                                            child: Icon(Icons.delete, color: Colors.red),
-                                            onPressed: () {
-                                              deletePost(item.id!)
-                                                  .then((value) => {
-                                                        if (value != null)
-                                                          {
-                                                            Flushbar(
-                                                              message: "Berhasil Delete",
-                                                              duration: Duration(seconds: 2),
-                                                              backgroundColor: Colors.redAccent,
-                                                              flushbarPosition: FlushbarPosition.TOP,
-                                                            ).show(context)
-                                                          }
-                                                      });
-                                              _getData();
-                                            },
-                                          )
-                                        ],
-                                      ),
+                                      // Row(
+                                      //   children: [
+                                      //     TextButton(
+                                      //       child: Icon(Icons.edit, color: Colors.yellow),
+                                      //       onPressed: () {
+                                      //         Navigator.push(
+                                      //           context,
+                                      //           MaterialPageRoute(
+                                      //             builder: (BuildContext context) =>
+                                      //                 EditPage(
+                                      //                   id: item.id,
+                                      //                   isiMotivasi: item.isiMotivasi,
+                                      //                   userid: '',
+                                      //                   idMotivasi: '',
+                                      //                 ),
+                                      //           ));
+                                      //       },
+                                      //     ),
+                                      //     TextButton(
+                                      //       child: Icon(Icons.delete, color: Colors.red),
+                                      //       onPressed: () {
+                                      //         deletePost(item.id!).then((value) => {
+                                      //           if (value != null) {
+                                      //             Flushbar(
+                                      //               message: "Berhasil Delete",
+                                      //               duration: Duration(seconds: 2),
+                                      //               backgroundColor: Colors.redAccent,
+                                      //               flushbarPosition: FlushbarPosition.TOP,
+                                      //             ).show(context)
+                                      //           }
+                                      //         });
+                                      //         _getData();
+                                      //       },
+                                      //     )
+                                      //   ],
+                                      // ),
                                     ],
                                   ),
                                 ),
